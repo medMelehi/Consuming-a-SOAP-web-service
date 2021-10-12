b4049fc3f4db957613b424459f966e28deed51f0
medMelehi <mohammadmelehi@gmail.com>
2021-10-12 21:39:09 +0100

    first commit

---
+++ src/test/java/com/sid/clientSoap/ClientSoapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sid.clientSoap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientSoapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
